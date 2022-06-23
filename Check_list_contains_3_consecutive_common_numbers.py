14401fdb9419ff4a7d29b2d3d07d34183fbfa4a1
sheetalwanghare49 <102980433+sheetalwanghare49@users.noreply.github.com>
2022-06-23 22:15:13 +0530

    Delete Check_list_contains_3_consecutive_common_numbers.py

--- Check_list_contains_3_consecutive_common_numbers.py
+++
@@ -1,8 +0,0 @@
-#To check if the list contains 3 consecutive common elements
-list = [6,4,2,1,7,7,7,9,0]
-print("list =",list)
-size = len(list)
-
-for i in range(size - 2):
-    if list[i] == list[i + 1] and list[i + 1] == list[i + 2]:
-        print("The 3 consecutive common element in list:",(list[i]))
