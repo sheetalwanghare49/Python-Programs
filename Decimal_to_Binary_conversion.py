3d034cf84521b17abca286117dd7720e7f18a88f
sheetalwanghare49 <102980433+sheetalwanghare49@users.noreply.github.com>
2022-06-23 22:07:01 +0530

    Delete Decimal_to_Binary_conversion.py

--- Decimal_to_Binary_conversion.py
+++
@@ -1,9 +0,0 @@
-#Program for convert a number from Decimal to Binary
-def convertToBinary(n):
-   if n > 1:
-       convertToBinary(n//2)
-   print(n % 2,end = '')
-
-dec=int(input("Enter the Number:"))
-convertToBinary(dec)
-print()
