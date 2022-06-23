0d712a4fe39c6ebc0bcb2bf690cadb124c7b88a7
sheetalwanghare49 <102980433+sheetalwanghare49@users.noreply.github.com>
2022-06-23 22:05:51 +0530

    Delete swap_two_variables.py

--- swap_two_variables.py
+++
@@ -1,9 +0,0 @@
-#Program to swap two variables
-x=int(input("Enter value of x:"))
-y=int(input("Enter value of y:"))
-
-temp=x
-x=y
-y=temp
-print("Value of x after swapping:",x)
-print("Value of y after swapping:",y)
