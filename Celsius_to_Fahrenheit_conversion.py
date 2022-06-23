b0f31012c691ec8781cde5f49d34cb5d1a8c0766
sheetalwanghare49 <102980433+sheetalwanghare49@users.noreply.github.com>
2022-06-23 22:06:48 +0530

    Delete Celsius_to_Fahrenheit_conversion.py

--- Celsius_to_Fahrenheit_conversion.py
+++
@@ -1,5 +0,0 @@
-#Program to convert Celsius to Fahrenheit
-celsius=float(input("Enter Temperature in degree celsius:"))
-
-fahrenheit=(celsius*1.8)+32
-print("%0.1f degree Celsius is equal to %0.1f degree Fahrenheit"%(celsius,fahrenheit))
