383e03293b7f9db88f6b3c0088873e791ba2b4ad
sheetalwanghare49 <102980433+sheetalwanghare49@users.noreply.github.com>
2022-06-23 22:09:29 +0530

    Delete concatenate_2_lists.py

--- concatenate_2_lists.py
+++
@@ -1,7 +0,0 @@
-#Python program which concatenate 2 lists
-list1 = ['p','q']
-print("list1:",list1)
-list2 = [1,2,3,4]
-print("list2:",list2)
-list = [x+str(y) for x in list1 for y in list2]
-print("list after concatenation:",list)
