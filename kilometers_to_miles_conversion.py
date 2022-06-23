efc6b56014023fae756c624b3bd956e053937bcf
sheetalwanghare49 <102980433+sheetalwanghare49@users.noreply.github.com>
2022-06-23 22:09:15 +0530

    Delete kilometers_to_miles_conversion.py

--- kilometers_to_miles_conversion.py
+++
@@ -1,5 +0,0 @@
-#Program to convert Kilometers to Miles
-a=float(input("Enter value in kilometers:"))
-conv_fac=0.621371
-miles=a*conv_fac
-print("%0.2f kilometers is equal to %0.2f miles"%(a,miles))
